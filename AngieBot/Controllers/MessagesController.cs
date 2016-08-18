719ee924f57ecdb118741a6db564c92bde9f6834
angie <t-anng@microsoft.com>
2016-08-18 13:44:46 -0700

    Made the bot a bit more cool it now tells your horoscope

--- AngieBot/Controllers/MessagesController.cs
+++ AngieBot/Controllers/MessagesController.cs
@@ -24,10 +24,56 @@ namespace AngieBot
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 // calculate something for us to return
                 int length = (activity.Text ?? string.Empty).Length;
+                bool understand = false;
+                
+                // Check if they said hey or hi or hello
+                string[] greetingArray = { "hey", "hello", "sup", "hi", "howdy" };
+                string[] horoscopeArray = { "aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces" };
+                foreach (string x in greetingArray)
+                {
+                    if (activity.Text.Contains(x))
+                    {
+                        Activity reply = activity.CreateReply($"Hello! I'm a simple bot made by t-anng that will tell you your horoscope for today. What is your sign?");
+                        await connector.Conversations.ReplyToActivityAsync(reply);
+                        understand = true;
+                    }
+                }
+                foreach (string x in horoscopeArray)
+                {
+                    if (activity.Text.Contains(x))
+                    {
+                        using (var client = new HttpClient())
+                        {
+                            client.DefaultRequestHeaders.Add("Accept", "application/json;odata=verbose");
+                            client.DefaultRequestHeaders.Add("ContentType", "application/json");
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                            HttpResponseMessage res = await client.GetAsync("http://horoscope-api.herokuapp.com/horoscope/today/" + x);
+                            if (res.IsSuccessStatusCode)
+                            {
+                                Console.WriteLine(res.StatusCode.ToString());
+                                string responseString = await res.Content.ReadAsStringAsync();
+                                Horoscope model = JsonConvert.DeserializeObject<Horoscope>(responseString);
+                                                               
+                                await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Wow, a " + model.sunsign + "!"));
+                                
+                                await connector.Conversations.ReplyToActivityAsync(activity.CreateReply(model.horoscope));
+                                
+                                await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Special thanks to this Horoscope API: http://tapasweni-pathak.github.io/Horoscope-API/"));
+                                                            }
+                            else
+                            {
+                                await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Something went wrong with fetching your horoscope. :("));
+                            }
+                        }
+                        understand = true;
+                    }
+                }
+                
+                if (!understand)
+                {
+                    Activity reply2 = activity.CreateReply($"Sorry, I can't understand what you're saying. What is your sign?");
+                    await connector.Conversations.ReplyToActivityAsync(reply2);
+                }
             }
             else
             {
---
+++ AngieBot/Horoscope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngieBot
+{
+    public class Horoscope
+    {
+        public string date { get; set; }
+
+        public string horoscope { get; set; }
+
+        public string sunsign { get; set; }
+    }
+}
