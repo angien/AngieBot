a5be8772bdc4c51dccdc3c63e13cd2e97e72c358
angie <t-anng@microsoft.com>
2016-08-18 14:57:14 -0700

    Added help option and lower case

--- AngieBot/Controllers/MessagesController.cs
+++ AngieBot/Controllers/MessagesController.cs
@@ -29,19 +29,25 @@ namespace AngieBot
                 // Check if they said hey or hi or hello
                 string[] greetingArray = { "hey", "hello", "sup", "hi", "howdy" };
                 string[] horoscopeArray = { "aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces" };
+
+                string uText = activity.Text.ToLower();
                 foreach (string x in greetingArray)
                 {
-                    if (activity.Text.Contains(x))
+                    if (uText.Contains(x))
                     {
                         Activity reply = activity.CreateReply($"Hello! I'm a simple bot made by t-anng that will tell you your horoscope for today. What is your sign?");
                         await connector.Conversations.ReplyToActivityAsync(reply);
+
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"(If you don't know your sign, say 'help')"));
                         understand = true;
                     }
                 }
                 foreach (string x in horoscopeArray)
                 {
-                    if (activity.Text.Contains(x))
+                    if (uText.Contains(x))
                     {
+
+                        await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Wow, a " + x + "!"));
                         using (var client = new HttpClient())
                         {
                             client.DefaultRequestHeaders.Add("Accept", "application/json;odata=verbose");
@@ -53,13 +59,10 @@ namespace AngieBot
                                 Console.WriteLine(res.StatusCode.ToString());
                                 string responseString = await res.Content.ReadAsStringAsync();
                                 Horoscope model = JsonConvert.DeserializeObject<Horoscope>(responseString);
-                                                               
-                                await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Wow, a " + model.sunsign + "!"));
-                                
+                                                
                                 await connector.Conversations.ReplyToActivityAsync(activity.CreateReply(model.horoscope));
-                                
                                 await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Special thanks to this Horoscope API: http://tapasweni-pathak.github.io/Horoscope-API/"));
-                                                            }
+                            }
                             else
                             {
                                 await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Something went wrong with fetching your horoscope. :("));
@@ -68,11 +71,32 @@ namespace AngieBot
                         understand = true;
                     }
                 }
+                if (uText.Contains("help"))
+                {
+                    Activity helpReply = activity.CreateReply($"Here's the list of all the zodiac signs.\n\n"
+                        + "Aries: March 21 - April 19\n\n"
+                        + "Taurus: April 20 - May 20\n\n"
+                        + "Gemini: May 21 - June 20\n\n"
+                        + "Cancer: June 21 - July 22\n\n"
+                        + "Leo: July 23 - August 22\n\n"
+                        + "Virgo: August 23 - Septemeber 22\n\n"
+                        + "Libra: September 23 - October 22\n\n"
+                        + "Scorpio: October 23 - November 21\n\n"
+                        + "Sagittarius: November 22 - December 21\n\n"
+                        + "Capricorn: December 22 - January 19\n\n"
+                        + "Aquarius: January 20 - February 18\n\n"
+                        + "Pisces: February 19 - March 20");
+
+                    await connector.Conversations.ReplyToActivityAsync(helpReply);
+                    understand = true;
+                }
                 
                 if (!understand)
                 {
-                    Activity reply2 = activity.CreateReply($"Sorry, I can't understand what you're saying. What is your sign?");
-                    await connector.Conversations.ReplyToActivityAsync(reply2);
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"Sorry, I couldn't understand what you said."));
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"I'm a simple bot made by t-anng that will tell you your horoscope for today. What is your sign?"));
+
+                    await connector.Conversations.ReplyToActivityAsync(activity.CreateReply($"(If you don't know your sign, say 'help'.)"));
                 }
             }
             else
